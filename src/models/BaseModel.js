713a900cb24f26afc9ef22c83c0a703e08769cdc
Luke Batchelor <lbatchelor@atlassian.com>
2018-01-30 14:13:21 +1100

    Removes the unused dependency on joi to prevent issues with node versions <8.9.0

--- src/models/BaseModel.js
+++ src/models/BaseModel.js
@@ -1,26 +1,26 @@
-// @flow
-import Joi from 'joi';
-import uuid from 'uuid';
+// // @flow
+// import Joi from 'joi';
+// import uuid from 'uuid';
 
-export default class BaseModel<Attributes> {
-  attrs: Attributes & { id: string };
-  static schema: Object;
+// export default class BaseModel<Attributes> {
+//   attrs: Attributes & { id: string };
+//   static schema: Object;
 
-  constructor(attrs: Attributes & { id: string }) {
-    this.constructor.validate(attrs);
-    this.attrs = attrs;
-  }
+//   constructor(attrs: Attributes & { id: string }) {
+//     this.constructor.validate(attrs);
+//     this.attrs = attrs;
+//   }
 
-  static create(attrs: Attributes) {
-    return new this.constructor({ ...attrs, id: uuid() });
-  }
+//   static create(attrs: Attributes) {
+//     return new this.constructor({ ...attrs, id: uuid() });
+//   }
 
-  toJSON() {
-    return this.attrs;
-  }
+//   toJSON() {
+//     return this.attrs;
+//   }
 
-  static validate(attrs: mixed) {
-    let result = Joi.validate(this.schema, attrs);
-    if (result.error) throw result.error;
-  }
-}
+//   static validate(attrs: mixed) {
+//     let result = Joi.validate(this.schema, attrs);
+//     if (result.error) throw result.error;
+//   }
+// }
--- src/models/LandRequest.js
+++ src/models/LandRequest.js
@@ -1,22 +1,22 @@
-// @flow
-import Joi from 'joi';
-import BaseModel from './BaseModel';
+// // @flow
+// import Joi from 'joi';
+// import BaseModel from './BaseModel';
 
-/**
- * I'm currently not using this and am instead just using a LandRequest type in ../types.js
- * We can look at adding these validations later
- */
+// /**
+//  * I'm currently not using this and am instead just using a LandRequest type in ../types.js
+//  * We can look at adding these validations later
+//  */
 
-type Attributes = {
-  pullRequestId: string,
-  commentId: string,
-  userId: string
-};
+// type Attributes = {
+//   pullRequestId: string,
+//   commentId: string,
+//   userId: string
+// };
 
-export default class LandRequest extends BaseModel<Attributes> {
-  static schema = Joi.object().keys({
-    pullRequestId: Joi.string().required(),
-    commentId: Joi.string().required(),
-    userId: Joi.string().required()
-  });
-}
+// export default class LandRequest extends BaseModel<Attributes> {
+//   static schema = Joi.object().keys({
+//     pullRequestId: Joi.string().required(),
+//     commentId: Joi.string().required(),
+//     userId: Joi.string().required()
+//   });
+// }
