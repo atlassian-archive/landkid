0ac9fbe5b2b2a5c64c564b9785bb6bc03c69ad50
Luke Batchelor <lbatchelor@atlassian.com>
2018-06-08 15:45:34 -0700

    Minor cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -7,12 +7,15 @@ const outputPath = process.env.OUTPUT_PATH || '.';
 module.exports = {
   entry: {
     bitbucket: path.resolve(__dirname, './src/static/bitbucket/index'),
-    'current-state': path.resolve(__dirname, './src/static/current-state/index')
+    'current-state': path.resolve(
+      __dirname,
+      './src/static/current-state/index',
+    ),
   },
   output: {
     path: path.resolve(outputPath),
     publicPath: '/',
-    filename: '[name]/bundle.js'
+    filename: '[name]/bundle.js',
   },
   mode: 'development',
   module: {
@@ -21,29 +24,38 @@ module.exports = {
         test: /\.js/,
         use: {
           loader: require.resolve('babel-loader'),
-          query: { presets: [require.resolve('babel-preset-react')] }
-        }
-      }
-    ]
+          query: { presets: [require.resolve('babel-preset-react')] },
+        },
+      },
+    ],
   },
   resolve: {
     alias: {
       react: 'preact-compat',
-      'react-dom': 'preact-compat'
-    }
+      'react-dom': 'preact-compat',
+    },
   },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'bitbucket/index.html',
       // only inject the code from the 'bitbucket' entry/chunk
       chunks: ['bitbucket'],
-      template: path.resolve(__dirname, './src/static/bitbucket/index.html')
+      template: path.resolve(__dirname, './src/static/bitbucket/index.html'),
     }),
     new HtmlWebpackPlugin({
       filename: 'current-state/index.html',
       // only inject the code from the 'current-state' entry/chunk
       chunks: ['current-state'],
-      template: path.resolve(__dirname, './src/static/current-state/index.html')
-    })
-  ]
+      template: path.resolve(
+        __dirname,
+        './src/static/current-state/index.html',
+      ),
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      // should't need any chunks for the home page
+      chunks: [],
+      template: path.resolve(__dirname, './src/static/index.html'),
+    }),
+  ],
 };
