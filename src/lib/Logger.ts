d9fa063693503d0b9c788a2bc7bd55e6ba03f329
Luke Batchelor <lbatchelor@atlassian.com>
2019-08-28 16:33:24 +1000

    Fixes Logger.ts again

--- src/lib/Logger.ts
+++ src/lib/Logger.ts
@@ -1,35 +1,18 @@
 import * as winston from 'winston';
-import * as path from 'path';
 
 process.stdout.isTTY = true;
 
-const LOG_DIR = path.resolve(process.cwd(), '.logs');
-
-export const Logger = winston.createLogger({
+const ProdLogger = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
-  transports: [
-    new winston.transports.File({
-      filename: path.resolve(LOG_DIR, 'landkid.error.log'),
-      level: 'error',
-    }),
-    new winston.transports.File({
-      filename: path.resolve(LOG_DIR, 'landkid.log'),
-    }),
-  ],
+  transports: [new winston.transports.Console()],
+});
+const DevelopmentLogger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
+  transports: [new winston.transports.Console()],
 });
 
-if (process.env.NODE_ENV !== 'production') {
-  Logger.add(
-    new winston.transports.Console({
-      // format: winston.format.simple(),
-      format: winston.format.combine(winston.format.colorize(), winston.format.simple()),
-    }),
-  );
-} else {
-  Logger.add(
-    new winston.transports.Console({
-      format: winston.format.json(),
-    }),
-  );
-}
+const LoggerToExport = process.env.NODE_ENV === 'production' ? ProdLogger : DevelopmentLogger;
+
+export const Logger = LoggerToExport;
