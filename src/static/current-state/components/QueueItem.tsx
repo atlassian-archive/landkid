8e90b90fee376de55ca417aa2ee0ad96e18d7587
gardnerjack <gardnerjack11@gmail.com>
2019-12-11 16:14:06 +1100

    Arrows

--- src/static/current-state/components/QueueItem.tsx
+++ src/static/current-state/components/QueueItem.tsx
@@ -171,7 +171,7 @@ export class QueueItem extends React.Component<QueueItemProps, QueueItemState> {
   render() {
     const {
       bitbucketBaseUrl,
-      request: { /*buildId,*/ pullRequestId, pullRequest, created },
+      request: { /*buildId,*/ pullRequestId, pullRequest },
       status,
     } = this.props;
 
@@ -225,13 +225,13 @@ export class QueueItem extends React.Component<QueueItemProps, QueueItemState> {
                 {distanceInWords(status.date, { addSuffix: true })}
               </StatusItem>
 
-              {['success', 'fail', 'aborted'].indexOf(status.state) !== -1 ? (
+              {/* {['success', 'fail', 'aborted'].indexOf(status.state) !== -1 ? (
                 <StatusItem title="Duration:">
                   <Lozenge appearance="new">
                     {duration(+new Date(created), +new Date(status.date))}
                   </Lozenge>
                 </StatusItem>
-              ) : null}
+              ) : null} */}
             </div>
           </ak-grid-column>
           <ak-grid-column size={1} style={{ alignSelf: 'center' }}>
@@ -254,16 +254,13 @@ export class QueueItem extends React.Component<QueueItemProps, QueueItemState> {
           </ak-grid-column>
         </ak-grid>
         {this.state.landRequestInfo ? (
-          <div className="queue-item__status-line">
+          <div className="queue-item__status-line" style={{ paddingLeft: '16px' }}>
             {this.state.landRequestInfo.statuses.map((status, index, statuses) => (
               <StatusItem
                 title={
                   index === 0
                     ? 'Status History:'
-                    : `-- ${duration(
-                        +new Date(statuses[index - 1].date),
-                        +new Date(status.date),
-                      )} -->`
+                    : `— ${duration(+new Date(statuses[index - 1].date), +new Date(status.date))} →`
                 }
               >
                 <Lozenge appearance={landStatusToAppearance[status.state]}>
