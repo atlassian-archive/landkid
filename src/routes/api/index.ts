cc4c10fa5d35a2d3a088248ac675d1c75d353e36
Raja Bellebon <rbellebon@gmail.com>
2020-09-16 12:12:50 +1000

    Adding /clear-land-when-able-queue API  (#86)
    * Adding /clear-land-when-able-queue API to avoid PR stalling in this queue
    
    * Replacing Landkid by Luke's && not fix
    
    Co-authored-by: raja <rbellebon@atlassian.com>

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -250,7 +250,8 @@ export function apiRoutes(runner: Runner, client: BitbucketClient, config: Confi
 
       const landRequest: LandRequestOptions = {
         prId,
-        triggererAaid: req.user ? req.user.aaid : 'landkid',
+        // Must always have a valid aaid, so lets set it to Luke's aaid
+        triggererAaid: req.user ? req.user.aaid : '557057:9512f4e4-3319-4d30-a78d-7d5f8ed243ae',
         commit: prInfo.commit,
         prTitle: prInfo.title,
         prAuthorAaid: prInfo.authorAaid,
@@ -275,6 +276,20 @@ export function apiRoutes(runner: Runner, client: BitbucketClient, config: Confi
       Logger.verbose('Deleting history', { namespace: 'routes:api:delete-history' });
       await runner.clearHistory();
       res.json({ response: 'You better be sure about this captain... Done!' });
+    }),
+  );
+
+  router.delete(
+    '/clear-land-when-able-queue',
+    requireAuth('admin'),
+    wrap(async (req, res) => {
+      Logger.verbose('Deleting land-when-able queue', {
+        namespace: 'routes:api:clear-land-when-able-queue',
+      });
+      await runner.clearLandWhenAbleQueue();
+      res.json({
+        response: 'You may not be a threat, but you better stop pretending to be a hero... Done!',
+      });
     }),
   );
 
