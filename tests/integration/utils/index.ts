884c409d0e4152dd17c6c4cb38e5ae769abb31f8
Luke Underwood <luke@snarkie.com.au>
2022-09-01 13:06:46 +0930

    Correct imports for `joi` in integration tests

--- tests/integration/utils/index.ts
+++ tests/integration/utils/index.ts
@@ -1,19 +1,20 @@
-import Joi from 'joi';
+import { string, array } from 'joi';
+import type { ArraySchema } from 'joi';
 
-const queued = Joi.string().only('queued');
-const running = Joi.string().only('running');
-const merging = Joi.string().only('merging');
-const fail = Joi.string().only('fail');
-const success = Joi.string().only('success');
+const queued = string().only('queued');
+const running = string().only('running');
+const merging = string().only('merging');
+const fail = string().only('fail');
+const success = string().only('success');
 
 const successfulFlow = [queued, running, merging, success];
 const failedFlow = [queued, running, fail];
 const failedDepFlow = [queued, running, fail];
 
-export const successful = Joi.array().ordered(...successfulFlow);
-export const failed = Joi.array().ordered(...failedFlow);
-export const reentryFail = Joi.array().ordered(...failedDepFlow, ...failedFlow);
-export const doubleReentrySuccess = Joi.array().ordered(
+export const successful = array().ordered(...successfulFlow);
+export const failed = array().ordered(...failedFlow);
+export const reentryFail = array().ordered(...failedDepFlow, ...failedFlow);
+export const doubleReentrySuccess = array().ordered(
   ...failedDepFlow,
   ...failedDepFlow,
   ...successfulFlow,
@@ -21,6 +22,6 @@ export const doubleReentrySuccess = Joi.array().ordered(
 
 // Including the `awaiting-merge` status check introduced flakiness
 // because of Bitbucket build times, merge times, and other factors
-export const validate = (statuses: string[], schema: Joi.ArraySchema) => {
+export const validate = (statuses: string[], schema: ArraySchema) => {
   return schema.validate(statuses.filter((status) => status !== 'awaiting-merge'));
 };
