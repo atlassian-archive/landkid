8bc67afe65eea2ea36a811b630da587cb9c2aab6
Shubham Kanodia <skanodia@atlassian.com>
2022-10-04 08:30:21 +0530

    Added stronger types for widget settings

--- src/static/bitbucket/utils/getWidgetSettings.ts
+++ src/static/bitbucket/utils/getWidgetSettings.ts
@@ -1,7 +1,6 @@
 import { WidgetSettings } from '../../../types';
 import { proxyRequestBare } from './RequestProxy';
-import useState from 'react-usestateref';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const defaultSettings = {
   refreshInterval: 10000,
@@ -12,7 +11,7 @@ export default function useWidgetSettings(): WidgetSettings {
   const [widgetSettings, setWidgetSettings] = useState<WidgetSettings>(defaultSettings);
 
   useEffect(() => {
-    proxyRequestBare<any>('/settings', 'POST')
+    proxyRequestBare<WidgetSettings>('/settings', 'POST')
       .then((settings) => {
         setWidgetSettings({ ...defaultSettings, ...settings });
       })
