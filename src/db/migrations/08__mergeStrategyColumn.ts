0521257f2946760d7ea45337a22a7e237da05ae5
Gautham Renganathan <grenganathan@atlassian.com>
2022-10-10 20:29:56 +0530

    fix migration query

--- src/db/migrations/08__mergeStrategyColumn.ts
+++ src/db/migrations/08__mergeStrategyColumn.ts
@@ -4,14 +4,24 @@ export default {
   up: function (query: QueryInterface) {
     return query.describeTable('LandRequest').then((table: any) => {
       if (table.mergeStrategy) return;
-      return query.sequelize.query(
-        'CREATE TYPE "enum_LandRequest_mergeStrategy" AS ENUM(\'squash\', \'merge-commit\'); ALTER TABLE "LandRequest" ADD COLUMN "mergeStrategy" "enum_LandRequest_mergeStrategy";',
-      );
+
+      return query.sequelize
+        .query('DROP TYPE IF EXISTS "enum_LandRequest_mergeStrategy";')
+        .then(() =>
+          query.sequelize.query(
+            "CREATE TYPE \"enum_LandRequest_mergeStrategy\" AS ENUM('squash', 'merge-commit');",
+          ),
+        )
+        .then(() =>
+          query.sequelize.query(
+            'ALTER TABLE "LandRequest" ADD COLUMN "mergeStrategy" "enum_LandRequest_mergeStrategy";',
+          ),
+        );
     });
   },
   down: function (query: QueryInterface) {
-    return query.sequelize.query(
-      'DROP TYPE "enum_LandRequest_mergeStrategy"; ALTER TABLE "LandRequest" DROP COLUMN "mergeStrategy";',
-    );
+    return query.sequelize
+      .query('DROP TYPE IF EXISTS "enum_LandRequest_mergeStrategy";')
+      .then(() => query.sequelize.query('ALTER TABLE "LandRequest" DROP COLUMN "mergeStrategy";'));
   },
 };
