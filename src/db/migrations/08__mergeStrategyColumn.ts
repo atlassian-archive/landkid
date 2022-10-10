1308edac174fa5762f130ef560712a6fe0f9067b
Gautham Renganathan <grenganathan@atlassian.com>
2022-10-10 18:58:25 +0530

    use direct queries

--- src/db/migrations/08__mergeStrategyColumn.ts
+++ src/db/migrations/08__mergeStrategyColumn.ts
@@ -1,19 +1,17 @@
 import { QueryInterface, DataTypes } from 'sequelize';
 
 export default {
-  up: function (query: QueryInterface, Sequelize: DataTypes) {
+  up: function (query: QueryInterface) {
     return query.describeTable('LandRequest').then((table: any) => {
       if (table.mergeStrategy) return;
-      return query.addColumn('LandRequest', 'mergeStrategy', {
-        type: Sequelize.ENUM({
-          values: ['squash', 'merge-commit'],
-        }),
-        allowNull: true,
-      });
+      return query.sequelize.query(
+        'CREATE TYPE "enum_LandRequest_mergeStrategy" AS ENUM(\'squash\', \'merge-commit\'); ALTER TABLE "LandRequest" ADD COLUMN "mergeStrategy" "enum_LandRequest_mergeStrategy";',
+      );
     });
   },
-  // VIOLATES FOREIGN KEY CONSTRAINT
-  down: function () {
-    throw new Error('NO DROP FUNCTION FOR THIS MIGRATION');
+  down: function (query: QueryInterface) {
+    return query.sequelize.query(
+      'DROP TYPE "enum_LandRequest_mergeStrategy"; ALTER TABLE "LandRequest" DROP COLUMN "mergeStrategy";',
+    );
   },
 };
