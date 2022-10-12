27a3bf76dcd929e3af6b6960c7707e11feae4161
grenganathan-atlassian <113331937+grenganathan-atlassian@users.noreply.github.com>
2022-10-12 11:20:17 +0530

    Fix migration script (#187)
    * use direct queries

--- src/db/migrations/08__mergeStrategyColumn.ts
+++ src/db/migrations/08__mergeStrategyColumn.ts
@@ -1,19 +1,27 @@
-import { QueryInterface, DataTypes } from 'sequelize';
+import { QueryInterface } from 'sequelize';
 
 export default {
-  up: function (query: QueryInterface, Sequelize: DataTypes) {
+  up: function (query: QueryInterface) {
     return query.describeTable('LandRequest').then((table: any) => {
       if (table.mergeStrategy) return;
-      return query.addColumn('LandRequest', 'mergeStrategy', {
-        type: Sequelize.ENUM({
-          values: ['squash', 'merge-commit'],
-        }),
-        allowNull: true,
-      });
+
+      return query.sequelize
+        .query('DROP TYPE IF EXISTS "enum_LandRequest_mergeStrategy";')
+        .then(() =>
+          query.sequelize.query(
+            "CREATE TYPE \"enum_LandRequest_mergeStrategy\" AS ENUM('squash', 'merge-commit');",
+          ),
+        )
+        .then(() =>
+          query.sequelize.query(
+            'ALTER TABLE "LandRequest" ADD COLUMN "mergeStrategy" "enum_LandRequest_mergeStrategy";',
+          ),
+        );
     });
   },
-  // VIOLATES FOREIGN KEY CONSTRAINT
-  down: function () {
-    throw new Error('NO DROP FUNCTION FOR THIS MIGRATION');
+  down: function (query: QueryInterface) {
+    return query.sequelize
+      .query('DROP TYPE IF EXISTS "enum_LandRequest_mergeStrategy";')
+      .then(() => query.sequelize.query('ALTER TABLE "LandRequest" DROP COLUMN "mergeStrategy";'));
   },
 };
