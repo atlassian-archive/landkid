ee52a152791c00458ccd800339aa5b7e2a3a9692
Michael Blaszczyk <mblaszczyk@atlassian.com>
2023-01-13 12:13:53 +1000

    Remove all cypress & integration test code

--- tests/integration/cypress.d.ts
+++
@@ -1,8 +0,0 @@
-declare namespace Cypress {
-  interface Chainable<Subject> {
-    visitLandkid(): void;
-    createLandRequest(title: string, isSuccessful: boolean): void;
-    waitForAllFinished(prTitles: string[], waitTime?: number): Promise<Record<string, any>>;
-    removePR(id: string, branchName: string): void;
-  }
-}
